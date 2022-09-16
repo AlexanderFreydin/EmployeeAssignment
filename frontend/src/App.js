626d8065d89b2a844d9e497a118cb53da073f268
AlexanderFreydin <alex@almatechnologies.com>
2022-09-16 08:36:38 +0300

    Front-end implemented: deleting and editing is not implemented yet

---
+++ frontend/src/App.js
@@ -0,0 +1,154 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from "react";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {employees: []};
+    this.updateTable = this.updateTable.bind(this);
+  }
+
+  async componentDidMount() {
+    const response = await fetch('/api/employees');
+    const body = await response.json();
+    this.setState({employees: body});
+  }
+
+    updateTable() {
+        console.log("update table")
+        this.componentDidMount();
+    }
+
+  render() {
+    return (
+        <EmployeeList employees={this.state.employees} updateTableCallback = {this.updateTable}/>
+    )
+  }
+}
+
+class EmployeeList extends React.Component{
+  render() {
+    const employees = this.props.employees.map(employee =>
+        <Employee key={employee.id} employee={employee}/>
+    );
+
+    return (
+        <div className="container">
+            <div className="table">
+                <div className="table-wrapper">
+                    <div className="table-title">
+                        <div className="row">
+                            <div className="col-sm-8"><h2>Employee <b>Details</b></h2></div>
+                            <div className="col-sm-4">
+
+                            </div>
+                        </div>
+                    </div>
+                    <table className="table table-striped table-hover table-bordered">
+                        <thead>
+                        <tr>
+                            <th>Id </th>
+                            <th>first name</th>
+                            <th>last name</th>
+                            <th>email</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {employees}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <CreationForm updateTableCallback = {this.props.updateTableCallback}/>
+        </div>
+
+
+    )
+  }
+}
+
+// class Employee extends React.Component{
+function Employee({updateTableCallback, employee}) {
+
+    const deleteEmployee =  (event) => {
+        event.preventDefault();
+        console.log(event.target);
+
+        // fetch('/api/employees', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(inputs)
+        // }).then(res => {
+        //     updateTableCallback();
+        // });
+    }
+
+
+    return (
+        <tr>
+          <td>{employee.id}</td>
+          <td>{employee.firstName}</td>
+          <td>{employee.lastName}</td>
+          <td>{employee.email}</td>
+            <td>
+                <a href="#" className="edit" title="Edit" data-toggle="tooltip"><i
+                    className="material-icons">&#xE254;</i></a>
+                <a href="#" className="delete" title="Delete" data-toggle="tooltip" onClick={deleteEmployee}><i
+                    className="material-icons">&#xE872;</i></a>
+            </td>
+        </tr>
+    )
+
+}
+
+function CreationForm({updateTableCallback}) {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit =  (event) => {
+        event.preventDefault();
+        console.log(inputs);
+
+        fetch('/api/employees', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        }).then(res => {
+            updateTableCallback();
+        });
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <div className="bold">Add Employee</div>
+
+        <div className="form-group">
+            <input type="text" className="form-control m-2" name="firstName" placeholder="First Name" onChange={handleChange}/>
+        </div>
+        <div className="form-group">
+            <input type="text" className="form-control m-2" name="lastName" placeholder="Last Name" onChange={handleChange}/>
+        </div>
+        <div className="form-group">
+            <input type="email" className="form-control m-2" name="email" aria-describedby="emailHelp"
+                   placeholder="Email" onChange={handleChange}/>
+        </div>
+        <button type="submit" className="btn btn-primary m-2">Submit</button>
+    </form>
+    )
+}
+
+export default App;
--- src/main/java/com/project/employee/controller/EmployeeController.java
+++ src/main/java/com/project/employee/controller/EmployeeController.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.project.employee.model.Employee;
 import com.project.employee.service.EmployeeService;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class EmployeeController {
     @Autowired
@@ -21,29 +17,75 @@ public class EmployeeController {
 
 //    Create employee
     @RequestMapping(value="/employees", method=RequestMethod.POST)
+    @ResponseBody
     public Employee createEmployee(@RequestBody Employee emp) {
         return empService.createEmployee(emp);
     }
 //    Get all employees
     @RequestMapping(value="/employees", method=RequestMethod.GET)
+    @ResponseBody
     public List<Employee> readEmployees() {
         return empService.getEmployees();
     }
 //    Get employee by id
     @RequestMapping(value="/employees/{empId}", method=RequestMethod.GET)
+    @ResponseBody
     public Employee readEmployees(@PathVariable(value = "empId") Long id) {
         return empService.getEmployeeById(id);
     }
 //    Update employee
     @RequestMapping(value="/employees/{empId}", method=RequestMethod.PUT)
+    @ResponseBody
     public Employee readEmployees(@PathVariable(value = "empId") Long id, @RequestBody Employee empDetails) {
         return empService.updateEmployee(id, empDetails);
     }
 //    delete employee
     @RequestMapping(value="/employees/{empId}", method=RequestMethod.DELETE)
+    @ResponseBody
     public void deleteEmployees(@PathVariable(value = "empId") Long id) {
         empService.deleteEmployee(id);
     }
 
+    //    Test
+    @RequestMapping(value="/test", method=RequestMethod.POST)
+    @ResponseBody
+    public customResponse test(@RequestBody Employee emp) {
+        customResponse customRes = new customResponse();
+        try {
+            Employee createdEmp = empService.createEmployee(emp);
+            customRes.setMessage("Successfully added employee");
+        }
+        catch (Exception e){
+            customRes.setMessage(e.getMessage());
+        }
+        return customRes;
+    }
 
+//    Request for UI
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+
+}
+
+
+class customResponse{
+    private String message;
+
+    public customResponse(){
+        this.message = "";
+    }
+    public customResponse(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
